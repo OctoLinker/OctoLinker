62448dea6e3581d299d6a612c9d715d84ff5b14a
Stefan Buck <dev@stefanbuck.com>
2014-07-30 00:28:15 +0200

    Fix #3 Resolve local require paths

--- app/scripts/module/require.js
+++ app/scripts/module/require.js
@@ -1,28 +1,18 @@
 'use strict';
 
 var util = require('util');
+var path = require('path');
 var $ = require('jquery');
 
 // List of nodejs core modules (v0.11.13)
 // see: https://github.com/joyent/node/blob/master/lib/repl.js#L72
 var coreModules = ['assert', 'buffer', 'child_process', 'cluster',
-  'crypto', 'dgram', 'dns', 'domain', 'events', 'fs', 'http', 'https', 'net',
-  'os', 'path', 'punycode', 'querystring', 'readline', 'stream',
-  'string_decoder', 'tls', 'tty', 'url', 'util', 'vm', 'zlib', 'smalloc',
-  'tracing'];
-// var GITHUBCOM = 'https://github.com/';
+'crypto', 'dgram', 'dns', 'domain', 'events', 'fs', 'http', 'https', 'net',
+'os', 'path', 'punycode', 'querystring', 'readline', 'stream',
+'string_decoder', 'tls', 'tty', 'url', 'util', 'vm', 'zlib', 'smalloc',
+'tracing'];
+var GITHUBCOM = 'https://github.com';
 var NODEAPICOM = 'http://nodejs.org/api/%s.html';
-
-/**
- * Returns the base url from the given repo url
- *
- * Input: https://github.com/stefanbuck/playground-repo/blob/master/index.js
- * Output: https://github.com/stefanbuck/playground-repo
- */
-// var getRepoBaseUrl = function(url) {
-//     url = url || location.href;
-//     return GITHUBCOM + url.replace(GITHUBCOM, '').split('/').slice(0,2).join('/');
-// };
 
 var getRequireLink = function(requireValue) {
 
@@ -38,12 +28,13 @@ var getRequireLink = function(requireValue) {
         link = npmRegistry[requireValue];
 
     } else {
-        // TODO https://github.com/stefanbuck/github-linker/issues/3
-        // var basePath = location.href.replace(GITHUBCOM, '');
-        // link = path.resolve(path.dirname(basePath), requireValue);
-        // if (!path.extname(link) ) {
-        //     link += '/index.js';
-        // }
+        var basePath = location.href.replace(GITHUBCOM, '');
+        var requirePath = path.resolve(path.dirname(basePath), requireValue);
+        if (path.extname(requirePath)) {
+            link = requirePath;
+        } else {
+            link = 'resolve:' + requirePath;
+        }
     }
 
     return link;
@@ -56,13 +47,52 @@ module.exports = function() {
         return $(this).text() === 'require';
     }).next().next();
 
-    $.each($requires, function(index, el) {
+    var attemptToLoadURL = function(urls, cb) {
+        var url = urls.shift();
+        $.ajax({
+            url: url,
+            type: 'HEAD'
+        }).then(function() {
+            cb(url);
+        }).fail(function() {
+            if (urls.length > 0) {
+                attemptToLoadURL(urls, cb);
+            } else {
+                cb(null);
+            }
+        });
+    };
 
-        var val = $(el).html().replace(/'|"/g, '');
-        var link = getRequireLink( val );
-
+    $( 'body' ).on( 'click', 'a.github-linker', function(e) {
+        var $el = $(this);
+        var link = $el.data('href');
         if (link) {
-            $(el).wrap('<a href="' + link + '">');
+            e.stopPropagation();
+            if (link.charAt(link.length-1) === '/') {
+                link = link.slice(0, -1);
+            }
+            var urls = [
+                GITHUBCOM + link+'.js',
+                GITHUBCOM + link+'/index.js',
+            ];
+            $('.page-context-loader').addClass('is-context-loading');
+            attemptToLoadURL(urls, function(link) {
+                $('.page-context-loader').removeClass('is-context-loading');
+                if (link) {
+                    location.href = link;
+                }
+            });
+        }
+    });
+
+    $.each($requires, function(index, el) {
+        var val = $(el).html().replace(/'|"/g, '');
+        var link = getRequireLink(val);
+        if ( link.indexOf('resolve:') === 0) {
+            link = link.replace('resolve:', '');
+            $(el).wrap('<a class="github-linker" data-href="' + link + '">');
+        } else {
+            $(el).wrap('<a class="github-linker" href="' + link + '">');
         }
     });
 };
