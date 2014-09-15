3e38bc049dcba2b54a3f669b0a33614a0e993221
Stefan Buck <web@broud.de>
2014-09-15 23:13:47 +0200

    initial commit

---
+++ app/scripts/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gitHubLinkerCore = require('github-linker-core');
+var $ = require('jquery');
+
+window.initGitHubLinker = function() {
+    gitHubLinkerCore($, window.location.href, function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+window.initGitHubLinker();
