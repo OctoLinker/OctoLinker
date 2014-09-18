75f3177d7473d5cfe66a38eb659e162ce8fe7475
Roland Warmerdam <rowno@webspirited.com>
2014-09-18 20:17:06 +1200

    Remove the unused pageAction

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,10 +4,6 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
-});
-
 /**
  * Get location from an url
  * @param  {String} url Url to parse
@@ -22,16 +18,11 @@ var getLocation = function(url) {
 };
 
 chrome.tabs.onUpdated.addListener(function( tabId, changeInfo, tab ) {
-
-    // Hide page action as defaul
-    chrome.pageAction.hide(tabId);
-
     if( changeInfo.status === 'complete' ) {
         if( tab && tab.url ) {
             // Check if we are at github
             var location = getLocation(tab.url);
             if (location && location.hostname && location.hostname.indexOf('github') !== -1) {
-                chrome.pageAction.show(tabId);
                 chrome.tabs.executeScript(null, {code: 'if (window.init) { window.init(); }'});
             }
         }
