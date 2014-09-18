b6a09e5b96a937d8e98141381e5c2df3ba55e920
Barry vd. Heuvel <barryvdh@gmail.com>
2014-09-18 17:02:51 +0200

    Update stats

--- tasks/writeStats.js
+++ tasks/writeStats.js
@@ -12,11 +12,14 @@ module.exports = function (grunt) {
 
     var totalBowerItems = grunt.config.get('totalBowerItems');
     var totalNPMItems = grunt.config.get('totalNPMItems');
+    var totalComposerItems = grunt.config.get('totalComposerItems');
 
     content = content.replace(/(npm: )[0-9]*/g, '$1'+ totalNPMItems)
       .replace(/(bower: )[0-9]*/g, '$1'+ totalBowerItems)
+      .replace(/(composer: )[0-9]*/g, '$1'+ totalComposerItems)
       .replace(/(badge\/npm-)[0-9]*/g, '$1'+ totalNPMItems)
-      .replace(/(badge\/bower-)[0-9]*/g, '$1'+ totalBowerItems);
+      .replace(/(badge\/bower-)[0-9]*/g, '$1'+ totalBowerItems)
+      .replace(/(badge\/composer-)[0-9]*/g, '$1'+ totalComposerItems);
 
     grunt.file.write(filename, content);
   });
