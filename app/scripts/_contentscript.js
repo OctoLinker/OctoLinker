c617413f74509736cf0c7c6d3d298a79bf931605
Stefan Buck <github@stefanbuck.com>
2015-01-28 07:50:07 +0100

    Disable update notification, at least until the next minor release

--- app/scripts/_contentscript.js
+++ app/scripts/_contentscript.js
@@ -4,15 +4,16 @@ var gitHubLinkerCore = require('github-linker-core');
 var $ = require('jquery');
 window.$ = $;
 
-chrome.storage.sync.get('showUpdateNotification', function(options) {
-    options = options || {};
+// chrome.storage.sync.get('showUpdateNotification', function(options) {
+var options = options || {};
 
-    options.changelog = 'https://github.com/github-linker/chrome-extension/releases';
-    options.version = require('../manifest.json').version.split('.').slice(0,-1).join('.');
+options.changelog = 'https://github.com/github-linker/chrome-extension/releases';
+options.version = require('../manifest.json').version.split('.').slice(0,-1).join('.');
+options.showUpdateNotification = false;
 
-    gitHubLinkerCore(window, options, function(err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+gitHubLinkerCore(window, options, function(err) {
+    if (err) {
+        console.error(err);
+    }
 });
+// });
