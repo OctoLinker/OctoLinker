51756393dd8ec475d06a5707aff447a72d44b3ec
Stefan Buck <github@stefanbuck.com>
2015-10-30 00:05:57 +0100

    Add chrome extension

--- src/github-linker.js
+++ src/github-linker.js
@@ -1,3 +1,9 @@
-export default function() {
-  return 'Hello';
-}
+import sourceReader from './sourceReader';
+
+console.time('sourceReader');
+
+const res = sourceReader();
+
+console.timeEnd('sourceReader');
+
+console.dir(res);
