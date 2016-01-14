b886c1cb1242979a7970092e667b5ffa03f286aa
Stefan Buck <github@stefanbuck.com>
2016-01-14 23:39:00 +0100

    Match imports over multiple lines

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -1,6 +1,6 @@
 const REQUIRE = /require(?:\s|\()(['"][^'"\s]+['"])\)?/g;
 const REQUIRE_RESOLVE = /require(?:.resolve)(?:\s|\()(['"][^'"\s]+['"])\)?/g;
-const IMPORT = /import\s+(?:.+\s+from\s+)?(['"][^'"\s]+['"])/g;
+const IMPORT = /import [\r\n\s\w{},*]*(?: from )?(['"][^'"\s]+['"])/g;
 
 export {
   REQUIRE,
