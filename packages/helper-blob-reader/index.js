84be122a38a0a4cb8ca2c1ac13f9f943c1141b54
Stefan Buck <github@stefanbuck.com>
2016-02-15 23:28:56 +0100

    Blob reader returns line numbers

--- packages/helper-blob-reader/index.js
+++ packages/helper-blob-reader/index.js
@@ -1,15 +1,70 @@
 import $ from 'jquery';
 import { languageByFilePath } from '../helper-file-type';
 
+function getLineNumber(el) {
+  // blob view
+  if (el.id) {
+    const result = /^LC?([0-9]+)$/.exec(el.id);
+    if (result && result[1]) {
+      return result[1];
+    }
+  }
+
+  // split diff view
+  let lineNumber = $(el).closest('td').prev().data('line-number');
+
+  // unified diff view
+  if (!lineNumber) {
+    lineNumber = $(el).closest('tr').find('td').data('line-number');
+  }
+
+  if (lineNumber) {
+    if (Number.isInteger(lineNumber)) {
+      return lineNumber;
+    }
+  }
+
+  return null;
+}
+
+function diffMetaInformation(el) {
+  const $td = $(el).closest('td');
+  return {
+    deletion: $td.hasClass('blob-code-deletion'),
+    addition: $td.hasClass('blob-code-addition'),
+  };
+}
+
 function readLine(el) {
+  const lineNumber = getLineNumber(el);
+
+  if (!lineNumber) {
+    return null;
+  }
+
+  const { deletion, addition } = diffMetaInformation(el);
+
   // Each array element represents a single line.
   // Therefore we can get ride of the newline here.
-  return $(el).text().replace(/\n/, '');
+  const ret = {
+    value: el.innerText.replace(/\n/, ''),
+    lineNumber,
+  };
+
+  if (deletion) {
+    ret.deletion = deletion;
+  }
+
+  if (addition) {
+    ret.addition = addition;
+  }
+
+  return ret;
 }
 
 function readBlobLines($blob) {
-  const $lines = $('.blob-code-inner', $blob);
-  return Array.prototype.map.call($lines, readLine);
+  const linesEl = $blob.getElementsByClassName('blob-code-inner');
+  return Array.prototype.map.call(linesEl, readLine).filter((line) => !!line);
 }
 
 function getBlobPath(blobElement) {
@@ -44,9 +99,9 @@ function getBlobMeta(blobElement) {
 }
 
 export default function () {
-  const $blobs = $('.blob-wrapper');
+  const blobs = document.getElementsByClassName('blob-wrapper');
 
-  return $.map($blobs, (el) => {
+  return $.map(blobs, (el) => {
     return getBlobMeta(el);
   });
 }
