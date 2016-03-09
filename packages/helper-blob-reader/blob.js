f0963b1b6efc55988b3c0dc82708725eea321c44
Stefan Buck <github@stefanbuck.com>
2016-03-09 21:55:50 +0100

    Add blob dirname and filename

--- packages/helper-blob-reader/blob.js
+++ packages/helper-blob-reader/blob.js
@@ -1,3 +1,4 @@
+import { basename, dirname } from 'path';
 import { getPath, getType, readLines } from './helper';
 
 export default class Blob {
@@ -10,6 +11,8 @@ export default class Blob {
 
     this.el = el;
     this.path = path;
+    this.filename = basename(path);
+    this.dirname = dirname(path);
     this.type = getType(this.path);
     this.lines = readLines(el);
   }
