josephfrazier/browser-extension

Add ruby support

lib/plugins/ruby/index.js
@@ -1,0 +1,20 @@
+import { REQUIRE } from '../../../packages/helper-grammar-regex-collection/index.js';
+import insertLink from '../../insert-link';
+import preset from '../../pattern-preset';
+
+export default class LiveResolver {
+
+  getPattern() {
+    return preset('Ruby');
+  }
+
+  parseBlob(blob) {
+    [REQUIRE].forEach((regex) => {
+      insertLink(blob.el, regex, {
+        resolver: 'rubyUniversal',
+        target: '$1',
+        path: blob.path,
+      });
+    });
+  }
+}

OctoLinker/OctoLinker

lib/plugins/ruby/index.js
@@ -1,0 +1,18 @@
+import { REQUIRE } from '../../../packages/helper-grammar-regex-collection/index.js';
+import insertLink from '../../insert-link';
+import preset from '../../pattern-preset';
+
+export default class Ruby {
+
+  getPattern() {
+    return preset('Ruby');
+  }
+
+  parseBlob(blob) {
+    insertLink(blob.el, REQUIRE, {
+      resolver: 'rubyUniversal',
+      target: '$1',
+      path: blob.path,
+    });
+  }
+}