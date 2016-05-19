0abc4b8dab9f0b65d156912e344ab0d08dd33e5a
James Talmage <james@talmage.io>
2016-05-19 03:25:40 -0400

    Allow for exported imports.
    The module spec allows for the following:
    
    ```js
    export * from "foo";
    export {foo, bar} from "foo";
    export {foo as bar} from "foo";
    ```
    
    Reference:
     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export#Syntax
     - http://www.ecma-international.org/ecma-262/6.0/#sec-exports
    
    Fixes #78

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -1,11 +1,13 @@
 const REQUIRE = /require(?:\s|\()(['"][^'"\s]+['"])\)?/g;
 const REQUIRE_RESOLVE = /require(?:.resolve)(?:\s|\()(['"][^'"\s]+['"])\)?/g;
 const IMPORT = /import [\r\n\s\w{},*\$]*(?: from )?(['"][^'"\s]+['"])/g;
+const EXPORT = /export [\r\n\s\w{},*\$]*(?: from )(['"][^'"\s]+['"])/g;
 const GEM = /gem (['"][^'"\s]+['"])/g;
 
 export {
   REQUIRE,
   REQUIRE_RESOLVE,
   IMPORT,
+  EXPORT,
   GEM,
 };
