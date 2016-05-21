8f6ed7abc29cb1bdb57f84b5d289c9f8ffa127c8
Joseph Frazier <1212jtraceur@gmail.com>
2016-05-20 22:10:23 -0400

    Support Homebrew-Cask dependencies/conflicts
    This is only for `cask:` dependencies/conflicts. Here's an example:
    
    https://github.com/caskroom/homebrew-cask/blob/d58e2c0724fe782a4ba18383e3dc387c59d63567/Casks/lazarus.rb#L11

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -3,7 +3,11 @@ const REQUIRE_RESOLVE = /require(?:.resolve)(?:\s|\()(['"][^'"\s]+['"])\)?/g;
 const IMPORT = /import [\r\n\s\w{},*\$]*(?: from )?(['"][^'"\s]+['"])/g;
 const EXPORT = /export [\r\n\s\w{},*\$]*(?: from )(['"][^'"\s]+['"])/g;
 const GEM = /gem (['"][^'"\s]+['"])/g;
-const HOMEBREW = /(?:depends_on|conflicts_with) (['"][^'"\s]+['"])/g;
+// TODO support `formula:` as well. See
+// https://github.com/caskroom/homebrew-cask/blob/714bd04bebd195500843dbb2cdbef21d65ff8852/doc/cask_language_reference/stanzas/depends_on.md#depends_on-formula
+// Ideally, we'd have a way of knowing which Homebrew tap is referred to, but
+// we could just use https://github.com/Homebrew/homebrew-core/
+const HOMEBREW = /(?:depends_on|conflicts_with)(?: cask:)? (['"][^'"\s]+['"])/g;
 
 export {
   REQUIRE,
