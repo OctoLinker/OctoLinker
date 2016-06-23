07e9897e0fbcd26138b23c039705c8e1aa03356c
Stefan Buck <github@stefanbuck.com>
2016-06-23 21:18:45 +0200

    Do not copy manifest file
    The manifest is already copied by the npm script task `chrome-manifest`

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,9 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([
       { from: 'assets' },
-    ]),
+    ], {
+      ignore: ['manifest.json'],
+    }),
   ],
   module: {
     loaders: [
