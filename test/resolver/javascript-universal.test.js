3d86b7c04b793364025af7fd656ee3300df36d6c
Joseph Frazier <1212jtraceur@gmail.com>
2016-06-30 18:09:47 -0400

    Test resolving targets like @angular/core/bar.js

--- test/resolver/javascript-universal.test.js
+++ test/resolver/javascript-universal.test.js
@@ -9,4 +9,15 @@ describe('javascript-universal', () => {
       javascriptUniversal({ type, target: 'foo' })
     );
   });
+
+  it("resolves '@angular/core/bar.js' to '@angular/core'", () => {
+    const type = 'npm';
+    assert.deepEqual(
+      javascriptUniversal({ type, target: '@angular/core/bar.js' })[0],
+      {
+        url: 'https://githublinker.herokuapp.com/q/npm/@angular/core',
+        method: 'GET',
+      }
+    );
+  });
 });
