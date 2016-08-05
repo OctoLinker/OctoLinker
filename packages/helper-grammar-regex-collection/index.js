a9666f2f670404953fe22ee2fe3e5d7ee1ee4378
Joseph Frazier <1212jtraceur@gmail.com>
2016-08-04 20:11:18 -0400

    Match literal dot in REQUIRE_RESOLVE regex (#150)
    * Add failing test for require.resolve regex
    
    * Match literal dot in REQUIRE_RESOLVE regex

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -1,5 +1,5 @@
 const REQUIRE = /require(?:\s|\()(['"][^'"\s]+['"])\)?/g;
-const REQUIRE_RESOLVE = /require(?:.resolve)(?:\s|\()(['"][^'"\s]+['"])\)?/g;
+const REQUIRE_RESOLVE = /require(?:\.resolve)(?:\s|\()(['"][^'"\s]+['"])\)?/g;
 const IMPORT = /import [\r\n\s\w{},*\$]*(?: from )?(['"][^'"\s]+['"])/g;
 const EXPORT = /export [\r\n\s\w{},*\$]*(?: from )(['"][^'"\s]+['"])/g;
 const GEM = /gem (['"][^'"\s]+['"])/g;
