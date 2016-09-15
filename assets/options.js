8c542499d53931dcc395c1ca9c2d88b158518cff
Joseph Frazier <1212jtraceur@gmail.com>
2016-09-15 10:06:13 -0400

    WIP remove checkbox from options page

--- assets/options.js
+++ assets/options.js
@@ -1,10 +1,8 @@
 // Saves options to chrome.storage.local.
 function save_options() {
   var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
   chrome.storage.local.set({
     favoriteColor: color,
-    likesColor: likesColor
   }, function() {
   });
 }
@@ -12,13 +10,11 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
+  // Use default value color = 'red'
   chrome.storage.local.get({
     favoriteColor: 'red',
-    likesColor: true
   }, function(items) {
     document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
