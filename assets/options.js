b443fc1a9fed91149649694ccdaa92f3f20edbe8
Joseph Frazier <1212jtraceur@gmail.com>
2016-09-15 10:05:12 -0400

    WIP Remove status text from options page

--- assets/options.js
+++ assets/options.js
@@ -6,12 +6,6 @@ function save_options() {
     favoriteColor: color,
     likesColor: likesColor
   }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
   });
 }
 
