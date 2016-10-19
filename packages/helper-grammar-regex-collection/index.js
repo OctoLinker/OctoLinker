beec18f5baf8bf9d5fba806b6e8a6b3caa0da692
Joseph Frazier <1212jtraceur@gmail.com>
2016-10-19 17:17:14 -0400

    helper-grammar-regex-collection: don't capture quotes (#201)
    * helper-grammar-regex-collection: test that quotes aren't captured
    
    * helper-grammar-regex-collection: don't capture quotes

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -1,11 +1,11 @@
-const REQUIRE = /require(?:\.resolve)?(?:\s|\()(['"][^'"\s]+['"])\)?/g;
-const IMPORT = /import [\r\n\s\w{},*\$]*(?: from )?(['"][^'"\s]+['"])/g;
-const EXPORT = /export [\r\n\s\w{},*\$]*(?: from )(['"][^'"\s]+['"])/g;
-const GEM = /gem (['"][^'"\s]+['"])/g;
-const HOMEBREW = /(?:depends_on|conflicts_with)(?: cask:| formula:)? (['"][^'"\s]+['"])/g;
-const TYPESCRIPT_REFERENCE = /\/{3}\s?<reference path=(['"][^'"\s]+['"])/g;
+const REQUIRE = /require(?:\.resolve)?(?:\s|\()['"]([^'"\s]+)['"]\)?/g;
+const IMPORT = /import [\r\n\s\w{},*\$]*(?: from )?['"]([^'"\s]+)['"]/g;
+const EXPORT = /export [\r\n\s\w{},*\$]*(?: from )['"]([^'"\s]+)['"]/g;
+const GEM = /gem ['"]([^'"\s]+)['"]/g;
+const HOMEBREW = /(?:depends_on|conflicts_with)(?: cask:| formula:)? ['"]([^'"\s]+)['"]/g;
+const TYPESCRIPT_REFERENCE = /\/{3}\s?<reference path=['"]([^'"\s]+)['"]/g;
 const DOCKER_FROM = /FROM\s([^\n]*)/g;
-const VIM_PLUGIN = /(?:(?:(?:Neo)?Bundle(?:Lazy|Fetch)?)|Plug(?:in)?)\s(['"][^'"\s]+['"])/g;
+const VIM_PLUGIN = /(?:(?:(?:Neo)?Bundle(?:Lazy|Fetch)?)|Plug(?:in)?)\s['"]([^'"\s]+)['"]/g;
 const RUST_CRATE = /(?:extern crate|use) ([^:; ]+)/g;
 const PYTHON_IMPORT = /(?:(?:\n|^)\s*import|from)\s([^\s]*)/g;
 
