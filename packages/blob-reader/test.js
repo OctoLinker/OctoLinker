3817f6e1e97ce30e883b8318af3a936c858829f9
Stefan Buck <github@stefanbuck.com>
2016-10-25 23:40:46 +0200

    Minor test setup tweaks

--- packages/blob-reader/test.js
+++ packages/blob-reader/test.js
@@ -12,47 +12,40 @@ describe('blob-reader', () => {
     beforeEach(() => {
       fixture.load('/packages/blob-reader/fixtures/github.com/blob/89f13651df126efdb4f1e3ae40183c9fdccdb4d3.html');
       const reader = new BlobReader();
-      reader.read().forEach((blob) => {
-        result = blob;
-      });
+      result = reader.read()._blobs[0];
     });
 
     describe('contains blob path', () => {
-      it('contains blob path', function () {
+      it('contains blob path', () => {
         assert.equal(result.path, '/octo-linker/testrepo/blob/89f13651df126efdb4f1e3ae40183c9fdccdb4d3/sourcereader/popular-rabbit-names.js');
       });
 
       it('when PR comment is up-to-date', () => {
         fixture.load('/packages/blob-reader/fixtures/github.com/pull/comments.html');
         const reader = new BlobReader();
-        reader.read().forEach((blob, index) => {
-          if (index !== 0) return;
-          assert.equal(blob.path, '/OctoLinker/testrepo/blob/64dc9c25b3e09d1d9af437e09d968d08ad5ec903/sourcereader/popular-cat-names.js');
-        });
+
+        assert.equal(reader.read()._blobs[0].path, '/OctoLinker/testrepo/blob/64dc9c25b3e09d1d9af437e09d968d08ad5ec903/sourcereader/popular-cat-names.js');
       });
 
       it('when PR comment is outdated', () => {
         fixture.load('/packages/blob-reader/fixtures/github.com/pull/comments.html');
         const reader = new BlobReader();
-        reader.read().forEach((blob, index) => {
-          if (index !== 1) return;
 
-          assert.equal(blob.path, '/OctoLinker/testrepo/blob/cc14b0ce8b94b7044f8c5d2d7af656270330bca2/sourcereader/popular-rabbit-names.js');
-        });
+        assert.equal(reader.read()._blobs[1].path, '/OctoLinker/testrepo/blob/cc14b0ce8b94b7044f8c5d2d7af656270330bca2/sourcereader/popular-rabbit-names.js');
       });
     });
 
-    it('contains blob root element', function () {
+    it('contains blob root element', () => {
       assert(result.el !== undefined);
     });
 
-    it('contains lines', function () {
+    it('contains lines', () => {
       assert(Array.isArray(result.lines));
       assert.equal(result.lines.length, 4);
     });
 
-    describe('getText', function () {
-      it('returns a string representation of the blobs content', function () {
+    describe('getText', () => {
+      it('returns a string representation of the blobs content', () => {
         result.lines = [
           { value: 'a' },
           { value: 'b' },
@@ -61,8 +54,8 @@ describe('blob-reader', () => {
       });
     });
 
-    describe('getJSON', function () {
-      it('returns a JSON representation of the blobs content', function () {
+    describe('getJSON', () => {
+      it('returns a JSON representation of the blobs content', () => {
         result.lines = [
           { value: '{' },
           { value: '"foo": "bar"' },
@@ -73,7 +66,7 @@ describe('blob-reader', () => {
         });
       });
 
-      it('returns an empty object if JSON.parse fails', function () {
+      it('returns an empty object if JSON.parse fails', () => {
         result.lines = [
           { value: '{' },
           { value: 'invalid' },
@@ -84,41 +77,39 @@ describe('blob-reader', () => {
     });
   });
 
-  describe('blob', function () {
+  describe('blob', () => {
     let result;
 
     beforeEach(() => {
       fixture.load('/packages/blob-reader/fixtures/github.com/blob/89f13651df126efdb4f1e3ae40183c9fdccdb4d3.html');
       const reader = new BlobReader();
-      reader.read().forEach((blob) => {
-        result = blob;
-      });
+      result = reader.read()._blobs[0];
     });
 
-    it('contains four lines', function () {
+    it('contains four lines', () => {
       assert.equal(result.lines.length, 4);
     });
 
-    it('does not contain any diff meta information', function () {
+    it('does not contain any diff meta information', () => {
       assert.equal(result.lines.filter((line) => line.additions || line.deletions).length, 0);
     });
 
-    it('1st line', function () {
+    it('1st line', () => {
       assert.equal(result.lines[0].lineNumber, 1);
       assert.equal(result.lines[0].value, '// Most popular rabbit names');
     });
 
-    it('2nd line', function () {
+    it('2nd line', () => {
       assert.equal(result.lines[1].lineNumber, 2);
       assert.equal(result.lines[1].value, '');
     });
 
-    it('3rd line', function () {
+    it('3rd line', () => {
       assert.equal(result.lines[2].lineNumber, 3);
       assert.equal(result.lines[2].value, 'Thumper');
     });
 
-    it('4th line', function () {
+    it('4th line', () => {
       assert.equal(result.lines[3].lineNumber, 4);
       assert.equal(result.lines[3].value, 'Daisy');
     });
@@ -131,17 +122,15 @@ describe('blob-reader', () => {
       beforeEach(() => {
         fixture.load('/packages/blob-reader/fixtures/github.com/commit/b0775a93ea27ee381858ddd9fa2bb953d5b74acb_split.html');
         const reader = new BlobReader();
-        reader.read().forEach((blob) => {
-          result = blob;
-        });
+        result = reader.read()._blobs[0];
       });
 
-      it('1st line', function () {
+      it('1st line', () => {
         assert.equal(result.lines[0].lineNumber, 1);
         assert.equal(result.lines[0].value, '// Most popular rabbit names');
       });
 
-      it('additions', function () {
+      it('additions', () => {
         const line = result.lines[6];
 
         assert.equal(line.lineNumber, 4);
@@ -149,7 +138,7 @@ describe('blob-reader', () => {
         assert.equal(line.addition, true);
       });
 
-      it('deletions', function () {
+      it('deletions', () => {
         const line = result.lines[9];
 
         assert.equal(line.lineNumber, 5);
@@ -164,21 +153,19 @@ describe('blob-reader', () => {
       beforeEach(() => {
         fixture.load('/packages/blob-reader/fixtures/github.com/commit/b0775a93ea27ee381858ddd9fa2bb953d5b74acb_unified.html');
         const reader = new BlobReader();
-        reader.read().forEach((blob) => {
-          result = blob;
-        });
+        result = reader.read()._blobs[0];
       });
 
-      it('contains four lines', function () {
+      it('contains four lines', () => {
         assert.equal(result.lines.length, 7);
       });
 
-      it('1st line', function () {
+      it('1st line', () => {
         assert.equal(result.lines[0].lineNumber, 1);
         assert.equal(result.lines[0].value, '// Most popular rabbit names');
       });
 
-      it('additions', function () {
+      it('additions', () => {
         const line = result.lines[3];
 
         assert.equal(line.lineNumber, 4);
@@ -186,7 +173,7 @@ describe('blob-reader', () => {
         assert.equal(line.addition, true);
       });
 
-      it('deletions', function () {
+      it('deletions', () => {
         const line = result.lines[5];
 
         assert.equal(line.lineNumber, 5);
@@ -202,12 +189,10 @@ describe('blob-reader', () => {
     beforeEach(() => {
       fixture.load('/packages/blob-reader/fixtures/github.com/gist/113827963013e98c6196db51cd889c39.html');
       const reader = new BlobReader();
-      reader.read().forEach((blob) => {
-        result = blob;
-      });
+      result = reader.read()._blobs[0];
     });
 
-    it('contains blob path', function () {
+    it('contains blob path', () => {
       assert.equal(result.path, 'package.json');
     });
   });
