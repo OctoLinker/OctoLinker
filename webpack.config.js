e23ab38e07fb53128e3370a9d5c66734ffa5a4c6
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-01 03:58:54 -0500

    Update dependencies (including webpack v2) (#272)
    npm-check -uE
        rm -rf node_modules/chokidar/node_modules/fsevents
        npm shrinkwrap
    
    Here's some info about the webpack.config.js changes:
    
    * https://webpack.js.org/guides/migrating/#automatic-loader-module-name-extension-removed
    * https://webpack.js.org/guides/migrating/#json-loader-is-not-required-anymore

--- webpack.config.js
+++ webpack.config.js
@@ -23,15 +23,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           plugins: ['transform-object-rest-spread'],
           presets: ['es2015'],
         },
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
       },
     ],
   },
