8072542d243317105022166d37ba740950d899bc
Stefan Buck <github@stefanbuck.com>
2017-02-08 21:34:00 +0100

    Refactor Rust plugin

--- test/plugins/rust.test.js
+++ test/plugins/rust.test.js
@@ -1,10 +1,10 @@
 import assert from 'assert';
-import rustCrate from '../../lib/resolver/rust-crate.js';
+import rustCrate from '../../lib/plugins/rust';
 
 describe('rust-crate', () => {
   it('resolves hamcrest using the live-resolver', () => {
     assert.deepEqual(
-      rustCrate({ target: 'hamcrest' }),
+      rustCrate.resolve({ target: 'hamcrest' }),
       {
         url: 'https://githublinker.herokuapp.com/q/crates/hamcrest',
         method: 'GET',
