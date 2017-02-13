d4dc63862b3675322faee1bb263c39175a8b6f20
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-13 17:46:16 -0500

    wip: captureQuotedDep

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -1,16 +1,20 @@
 import XRegExp from 'xregexp';
 import go from './go.js';
 
-const REQUIRE = XRegExp.build(`require(?:\\.resolve)?(?:\\s|\\()\\s*['"]([^'"\\s]+)['"]\\s*\\)?`, null, 'g');
-const IMPORT = XRegExp.build(`import [\\r\\n\\s\\w{},*\\$]*(?: from )?['"]([^'"\\s]+)['"]`, null, 'g');
-const EXPORT = XRegExp.build(`export [\\r\\n\\s\\w{},*\\$]*(?: from )['"]([^'"\\s]+)['"]`, null, 'g');
-const GEM = XRegExp.build(`gem ['"]([^'"\\s]+)['"]`, null, 'g');
-const HOMEBREW = XRegExp.build(`(?:depends_on|conflicts_with)(?: cask:| formula:)? ['"]([^'"\\s]+)['"]`, null, 'g');
-const TYPESCRIPT_REFERENCE = XRegExp.build(`\\/{3}\\s?<reference path=['"]([^'"\\s]+)['"]`, null, 'g');
-const DOCKER_FROM = XRegExp.build(`FROM\\s([^\\n]*)`, null, 'g');
-const VIM_PLUGIN = XRegExp.build(`(?:(?:(?:Neo)?Bundle(?:Lazy|Fetch)?)|Plug(?:in)?)\\s['"]([^'"\\s]+)['"]`, null, 'g');
-const RUST_CRATE = XRegExp.build(`(?:extern crate|use) ([^:; ]+)`, null, 'g');
-const PYTHON_IMPORT = XRegExp.build(`^\\s*(?:import|from)\\s([^\\s]*)`, null, 'gm');
+const subpatterns = {
+  captureQuotedDep: /['"]([^'"\s]+)['"]/,
+};
+
+const REQUIRE = XRegExp.build(`require(?:\\.resolve)?(?:\\s|\\()\\s*{{captureQuotedDep}}\\s*\\)?`, subpatterns, 'g');
+const IMPORT = XRegExp.build(`import [\\r\\n\\s\\w{},*\\$]*(?: from )?{{captureQuotedDep}}`, subpatterns, 'g');
+const EXPORT = XRegExp.build(`export [\\r\\n\\s\\w{},*\\$]*(?: from ){{captureQuotedDep}}`, subpatterns, 'g');
+const GEM = XRegExp.build(`gem {{captureQuotedDep}}`, subpatterns, 'g');
+const HOMEBREW = XRegExp.build(`(?:depends_on|conflicts_with)(?: cask:| formula:)? {{captureQuotedDep}}`, subpatterns, 'g');
+const TYPESCRIPT_REFERENCE = XRegExp.build(`\\/{3}\\s?<reference path={{captureQuotedDep}}`, subpatterns, 'g');
+const DOCKER_FROM = XRegExp.build(`FROM\\s([^\\n]*)`, subpatterns, 'g');
+const VIM_PLUGIN = XRegExp.build(`(?:(?:(?:Neo)?Bundle(?:Lazy|Fetch)?)|Plug(?:in)?)\\s{{captureQuotedDep}}`, subpatterns, 'g');
+const RUST_CRATE = XRegExp.build(`(?:extern crate|use) ([^:; ]+)`, subpatterns, 'g');
+const PYTHON_IMPORT = XRegExp.build(`^\\s*(?:import|from)\\s([^\\s]*)`, subpatterns, 'gm');
 
 export {
   REQUIRE,
