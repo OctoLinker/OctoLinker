cb0f9f6bb6afc011d83f7850fe0b2f8fdb42b310
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-14 18:22:07 -0500

    wip: use fancy tagged template literal
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#Tagged_template_literals

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -17,46 +17,48 @@ const subpatterns = {
   importMembers: /[\r\n\s\w{},*\$]*/,
 };
 
-const REQUIRE = XRegExp.build(raw`(?nx)
+const regex = pattern => XRegExp.build(`(?nx)${raw(pattern)}`, subpatterns, 'gm');
+
+const REQUIRE = regex`
   require(\.resolve)?
   ( \s | \( ) \s*
   {{captureQuotedDep}}
   \s* \)?
-  `, subpatterns, 'g');
+`;
 
-const IMPORT = XRegExp.build(raw`(?nx)
+const IMPORT = regex`
   import \s {{importMembers}}
   ( \s from \s )?
   {{captureQuotedDep}}
-  `, subpatterns, 'g');
+`;
 
-const EXPORT = XRegExp.build(raw`(?nx)
+const EXPORT = regex`
   export \s {{importMembers}}
   ( \s from \s )
   {{captureQuotedDep}}
-  `, subpatterns, 'g');
+`;
 
-const GEM = XRegExp.build(raw`(?nx)
+const GEM = regex`
   gem \s {{captureQuotedDep}}
-  `, subpatterns, 'g');
+`;
 
-const HOMEBREW = XRegExp.build(raw`(?nx)
+const HOMEBREW = regex`
   (depends_on|conflicts_with)
   ( \s cask: | \s formula: )?
   \s
   {{captureQuotedDep}}
-  `, subpatterns, 'g');
+`;
 
-const TYPESCRIPT_REFERENCE = XRegExp.build(raw`(?nx)
+const TYPESCRIPT_REFERENCE = regex`
   \/{3} \s?
   <reference \s path={{captureQuotedDep}}
-  `, subpatterns, 'g');
+`;
 
-const DOCKER_FROM = XRegExp.build(raw`(?nx)
+const DOCKER_FROM = regex`
   FROM \s (?<$1>[^\n]*)
-  `, subpatterns, 'g');
+`;
 
-const VIM_PLUGIN = XRegExp.build(raw`(?nx)
+const VIM_PLUGIN = regex`
   (
     (
       (Neo)?
@@ -67,20 +69,20 @@ const VIM_PLUGIN = XRegExp.build(raw`(?nx)
     Plug(in)?
   ) \s
   {{captureQuotedDep}}
-  `, subpatterns, 'g');
+`;
 
-const RUST_CRATE = XRegExp.build(raw`(?nx)
+const RUST_CRATE = regex`
   (extern \s crate | use)
   \s
   (?<$1>[^:;\s]+)
-  `, subpatterns, 'g');
+`;
 
-const PYTHON_IMPORT = XRegExp.build(raw`(?nx)
+const PYTHON_IMPORT = regex`
   ^\s*
   (import|from)
   \s
   (?<$1>[^\s]*)
-  `, subpatterns, 'gm');
+`;
 
 export {
   REQUIRE,
