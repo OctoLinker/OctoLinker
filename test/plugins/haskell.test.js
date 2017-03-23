0723fd4fe13d33ea60a304261dc84d335895fb79
Stefan Buck <github@stefanbuck.com>
2017-03-23 22:25:31 +0100

    Use github search resolver as fallback for haskell

--- test/plugins/haskell.test.js
+++ test/plugins/haskell.test.js
@@ -1,16 +1,19 @@
 import assert from 'assert';
 import Haskell from '../../lib/plugins/haskell';
+import githubSearch from '../../lib/resolver/github-search.js';
 
 describe('haskell', () => {
   const path = '/user/repo/blob/d6/lib/plugins/javascript.js';
+  const target = 'Foo.Bar';
 
   it('resolves links', () => {
     assert.deepEqual(
-      Haskell.resolve({ path, target: 'Foo.Bar' }),
+      Haskell.resolve({ path, target }),
       [
         '{BASE_URL}/user/repo/blob/master/src/Foo/Bar.hs',
         '{BASE_URL}/user/repo/blob/master/lib/Foo/Bar.hs',
         '{BASE_URL}/user/repo/blob/master/Foo/Bar.hs',
+        githubSearch({ path, target }).toString(),
         'https://hackage.haskell.org/package/base/docs/Foo-Bar.html',
       ],
     );
