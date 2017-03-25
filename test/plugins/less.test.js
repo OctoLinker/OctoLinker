7c785a581facd284519c70f2abad18b11855d8ce
Stefan Buck <github@stefanbuck.com>
2017-03-25 08:23:44 +0100

    Fallback to fuzzy search (#327)
    * Add github search resolver
    
    * Use github search resolver as fallback for less
    
    * Use github search resolver as fallback for sass
    
    * Use github search resolver as fallback for haskell

--- test/plugins/less.test.js
+++ test/plugins/less.test.js
@@ -1,14 +1,17 @@
 import assert from 'assert';
 import Less from '../../lib/plugins/less';
+import githubSearch from '../../lib/resolver/github-search.js';
 
 describe('Less', () => {
   const path = '/octo/dog.less';
+  const target = 'foo.less';
 
   it('resolves links', () => {
     assert.deepEqual(
-      Less.resolve({ path, target: 'foo.less' }),
+      Less.resolve({ path, target }),
       [
         '{BASE_URL}/octo/foo.less',
+        githubSearch({ path, target }).toString(),
       ],
     );
   });
