b2ea310612279bcce647dd04528d6c0e318ef13e
Joseph Frazier <1212jtraceur@gmail.com>
2017-04-07 18:41:32 -0400

    Make plugins regular objects instead of classes
    This lets us avoid unnecessarily instantiating the class, and just use
    the plugin directly instead.

--- test/load-plugins.test.js
+++ test/load-plugins.test.js
@@ -2,13 +2,17 @@ import assert from 'assert';
 import loadPlugins from '../lib/load-plugins.js';
 
 describe('load-plugins', () => {
-  it('returns an array of functions', () => {
+  it('returns an array of objects', () => {
     const plugins = loadPlugins();
 
     assert(Array.isArray(plugins));
 
-    plugins.forEach((func) => {
-      assert.equal(typeof func, 'function');
+    plugins.forEach((plugin) => {
+      assert.equal(typeof plugin, 'object');
+      assert(plugin.name);
+      assert(plugin.resolve);
+      assert(plugin.getPattern);
+      assert(plugin.parseBlob || plugin.getLinkRegexes);
     });
   });
 });
