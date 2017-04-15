fbb7773449cd059e2851f45dbb296dfb2e7ec367
Joseph Frazier <1212jtraceur@gmail.com>
2017-04-15 16:58:50 -0400

    vim: Use `target` argument instead of `shorthand`
    Commit 9750462 accidentally broke vimrc support, but this fixes it.
    
    Test page: https://github.com/gmarik/vimfiles/blob/1f4f26d42f54443f1158e0009746a56b9a28b053/vimrc#L140-L144

--- test/plugins/vim.test.js
+++ test/plugins/vim.test.js
@@ -4,21 +4,21 @@ import vimPlugin from '../../lib/plugins/vim';
 describe('vim-plugin', () => {
   it('resolves VundleVim/Vundle.vim to https://github.com/VundleVim/Vundle.vim', () => {
     assert.deepEqual(
-      vimPlugin.resolve({ shorthand: 'VundleVim/Vundle.vim' }),
+      vimPlugin.resolve({ target: 'VundleVim/Vundle.vim' }),
       'https://github.com/VundleVim/Vundle.vim',
     );
   });
 
   it('resolves ctrlp.vim to https://github.com/vim-scripts/ctrlp.vim', () => {
     assert.deepEqual(
-      vimPlugin.resolve({ shorthand: 'ctrlp.vim' }),
+      vimPlugin.resolve({ target: 'ctrlp.vim' }),
       'https://github.com/vim-scripts/ctrlp.vim',
     );
   });
 
   it('resolves a git:// URL to https/http', () => {
     assert.deepEqual(
-      vimPlugin.resolve({ shorthand: 'git://git.wincent.com/command-t.git' }),
+      vimPlugin.resolve({ target: 'git://git.wincent.com/command-t.git' }),
       'http://git.wincent.com/command-t',
     );
   });
