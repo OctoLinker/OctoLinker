7a12c2665ba5d5999d6cb4786449eef996d33b91
Joseph Frazier <1212jtraceur@gmail.com>
2017-05-16 12:30:20 -0400

    Link `proxyquire` like `require` (#361)
    For example, `prettier/bin/prettier` should link to the `prettier` repo in this code:
    
    ```js
    module.exports = proxyquire("prettier/bin/prettier", { "../index": require("../") });
    ```

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -12,7 +12,7 @@ const importMembers = regex`[\r\n\s\w{},*\$]*`;
 const from = regex`\s from \s`;
 
 export const REQUIRE = regex`
-  ( require(\.resolve)? | import | require_relative )
+  ( require(\.resolve)? | proxyquire | import | require_relative )
   \s* ( \s | \( ) \s*
   ${captureQuotedWord}
 `;
