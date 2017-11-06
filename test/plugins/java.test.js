fad707d680b3583ae3b8c02e86b75e1b8529c342
Stefan Buck <dev@stefanbuck.com>
2017-11-07 00:21:59 +0100

    Link java core packages to the related documentation page

---
+++ test/plugins/java.test.js
@@ -0,0 +1,15 @@
+import assert from 'assert';
+import Java from '../../lib/plugins/java';
+
+describe('Java', () => {
+  it('resolves java core links', () => {
+    assert.deepEqual(Java.resolve({ target: 'java.util.Foo' }), [
+      `https://docs.oracle.com/javase/9/docs/api/java/util/Foo.html`,
+      `https://docs.oracle.com/javaee/9/api/java/util/Foo.html`,
+      `https://docs.oracle.com/javase/8/docs/api/java/util/Foo.html`,
+      `https://docs.oracle.com/javaee/8/api/java/util/Foo.html`,
+      `https://docs.oracle.com/javase/7/docs/api/java/util/Foo.html`,
+      `https://docs.oracle.com/javaee/7/api/java/util/Foo.html`,
+    ]);
+  });
+});
