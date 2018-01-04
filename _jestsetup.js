f80701df8b1ccb3bb4d2831fbff7034858687f0f
Stefan Buck <dev@stefanbuck.com>
2018-01-04 21:31:59 +0100

    Move tests file

--- _jestsetup.js
+++ _jestsetup.js
@@ -10,7 +10,7 @@ global.chrome.runtime = {
 
 global.fixture = {
   load: file => {
-    const fullPath = path.join(__dirname, '..', file);
+    const fullPath = path.join(__dirname, file);
     const fixture = fs.readFileSync(fullPath);
     document.body.innerHTML = fixture.toString();
   },
