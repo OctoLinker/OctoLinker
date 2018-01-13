a4cb58361a8be7eb1799be2d9c3056f67b68b92a
Stefan Buck <dev@stefanbuck.com>
2018-01-13 15:05:39 +0100

    Make eslint working with mono repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     'no-prototype-builtins': 0,
     'import/extensions': 0,
     'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': 0,
     'no-underscore-dangle': 0,
     'no-useless-escape': 0,
     'class-methods-use-this': 0,
