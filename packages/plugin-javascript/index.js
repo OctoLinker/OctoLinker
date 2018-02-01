e905eccf4c32405dce69ce79b6a92d3597b1d980
Stefan Buck <dev@stefanbuck.com>
2018-02-01 21:27:25 +0100

    Move buildins to package plugin-javascript

--- packages/plugin-javascript/index.js
+++ packages/plugin-javascript/index.js
@@ -5,8 +5,8 @@ import {
   IMPORT,
   EXPORT,
 } from '@octolinker/helper-grammar-regex-collection';
-import builtinsDocs from '../utils/builtins-docs.js';
 import liveResolverQuery from '@octolinker/resolver-live-resolver-query';
+import builtinsDocs from './builtins-docs.js';
 
 function getTopModuleName(target) {
   const isScoped = target.startsWith('@');
