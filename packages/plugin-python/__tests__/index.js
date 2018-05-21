ab60c2c4d0edecd2634b3938a012fbed82c99fc3
Stefan Buck <dev@stefanbuck.com>
2018-05-21 23:14:29 +0200

    Extend local Pyhton import support

--- packages/plugin-python/__tests__/index.js
+++ packages/plugin-python/__tests__/index.js
@@ -5,35 +5,76 @@ import python from '../index';
 describe('python', () => {
   const path = '/octo/dog.py';
 
-  it('resolves local file', () => {
-    assert.deepEqual(python.resolve(path, ['.foo']), '{BASE_URL}/octo/foo.py');
+  describe('local file', () => {
+    it('import .foo', () => {
+      assert.deepEqual(python.resolve(path, ['.foo']), [
+        '{BASE_URL}/octo/foo.py',
+        '{BASE_URL}/octo/foo/__init__.py',
+      ]);
+    });
+
+    it('from .foo import bar', () => {
+      assert.deepEqual(python.resolve(path, ['.foo', 'bar']), [
+        '{BASE_URL}/octo/foo/bar.py',
+        '{BASE_URL}/octo/foo.py',
+        '{BASE_URL}/octo/foo/__init__.py',
+      ]);
+    });
+
+    it('from .foo.bar import baz', () => {
+      assert.deepEqual(python.resolve(path, ['.foo.bar', 'baz']), [
+        '{BASE_URL}/octo/foo/bar/baz.py',
+        '{BASE_URL}/octo/foo/bar.py',
+        '{BASE_URL}/octo/foo/bar/__init__.py',
+      ]);
+    });
+
+    it('from . import foo', () => {
+      assert.deepEqual(python.resolve(path, ['.', 'foo']), [
+        '{BASE_URL}/octo/foo.py',
+        '{BASE_URL}/octo/foo/__init__.py',
+      ]);
+    });
+
+    it('import foo.bar', () => {
+      assert.deepEqual(
+        python.resolve(path, ['foo.bar'])[0],
+        '{BASE_URL}/octo/foo/bar.py',
+      );
+    });
+
+    it('import .', () => {
+      assert.deepEqual(
+        python.resolve(path, ['.']),
+        '{BASE_URL}/octo/__init__.py',
+      );
+    });
   });
 
-  it('resolves init file', () => {
-    assert.deepEqual(
-      python.resolve(path, ['.']),
-      '{BASE_URL}/octo/__init__.py',
-    );
-  });
-
-  it('resolves package', () => {
-    assert.deepEqual(
-      python.resolve(path, ['foo'])[1],
-      liveResolverQuery({ type: 'pypi', target: 'foo' }),
-    );
-  });
-
-  it('resolves scope package', () => {
-    assert.deepEqual(
-      python.resolve(path, ['foo.bar'])[1],
-      liveResolverQuery({ type: 'pypi', target: 'foo' }),
-    );
-  });
-
-  it('resolves buildin', () => {
-    assert.deepEqual(
-      python.resolve(path, ['foo'])[0],
+  it('import foo', () => {
+    assert.deepEqual(python.resolve(path, ['foo']), [
+      '{BASE_URL}/octo/foo.py',
       'https://docs.python.org/3/library/foo.html',
-    );
+      liveResolverQuery({ type: 'pypi', target: 'foo' }),
+    ]);
+  });
+
+  it('import foo.bar', () => {
+    assert.deepEqual(python.resolve(path, ['foo.bar']), [
+      '{BASE_URL}/octo/foo/bar.py',
+      'https://docs.python.org/3/library/foo.bar.html',
+      liveResolverQuery({ type: 'pypi', target: 'foo' }),
+    ]);
+  });
+
+  it('from foo.bar import baz', () => {
+    assert.deepEqual(python.resolve(path, ['foo.bar', 'baz']), [
+      '{BASE_URL}/octo/foo/bar/baz.py',
+      '{BASE_URL}/octo/foo/bar/baz/__init__.py',
+      '{BASE_URL}/octo/foo/bar.py',
+      '{BASE_URL}/octo/foo/bar/__init__.py',
+      'https://docs.python.org/3/library/foo.bar.html',
+      liveResolverQuery({ type: 'pypi', target: 'foo' }),
+    ]);
   });
 });
--- packages/plugin-python/index.js
+++ packages/plugin-python/index.js
@@ -5,26 +5,93 @@ import relativeFile from '@octolinker/resolver-relative-file';
 export default {
   name: 'Python',
 
-  resolve(path, [target]) {
-    const isLocalFile = target.startsWith('.') && target.length > 1;
-    const isInit = target === '.';
+  resolve(path, [target, subTarget]) {
+    const isLocalFile = target.startsWith('.');
+    const normalizedTarget = target.replace(/\./g, '/');
     const apiDoc = `https://docs.python.org/3/library/${target}.html`;
 
     if (isLocalFile) {
-      return relativeFile({
-        target: `${target.slice(1)}.py`,
-        path,
-      });
+      const newTarget = target.slice(1);
+      if (target === '.' && !subTarget) {
+        return relativeFile({
+          target: '__init__.py',
+          path,
+        });
+      }
+
+      if (target === '.' && subTarget) {
+        return [
+          relativeFile({
+            target: `${newTarget}/${subTarget}.py`,
+            path,
+          }),
+          relativeFile({
+            target: `${newTarget}/${subTarget}/__init__.py`,
+            path,
+          }),
+        ];
+      }
+
+      if (subTarget) {
+        return [
+          relativeFile({
+            target: `${normalizedTarget}/${subTarget}.py`,
+            path,
+          }),
+          relativeFile({
+            target: `${normalizedTarget}.py`,
+            path,
+          }),
+          relativeFile({
+            target: `${normalizedTarget}/__init__.py`,
+            path,
+          }),
+        ];
+      }
+
+      return [
+        relativeFile({
+          target: `${normalizedTarget}.py`,
+          path,
+        }),
+        relativeFile({
+          target: `${normalizedTarget}/__init__.py`,
+          path,
+        }),
+      ];
     }
 
-    if (isInit) {
-      return relativeFile({
-        target: '__init__.py',
-        path,
-      });
+    if (subTarget) {
+      return [
+        relativeFile({
+          target: `${normalizedTarget}/${subTarget}.py`,
+          path,
+        }),
+        relativeFile({
+          target: `${normalizedTarget}/${subTarget}/__init__.py`,
+          path,
+        }),
+        relativeFile({
+          target: `${normalizedTarget}.py`,
+          path,
+        }),
+        relativeFile({
+          target: `${normalizedTarget}/__init__.py`,
+          path,
+        }),
+        apiDoc,
+        liveResolverQuery({
+          target: target.split('.')[0],
+          type: 'pypi',
+        }),
+      ];
     }
 
     return [
+      relativeFile({
+        target: `${normalizedTarget}.py`,
+        path,
+      }),
       apiDoc,
       liveResolverQuery({
         target: target.split('.')[0],
