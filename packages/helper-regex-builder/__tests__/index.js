1e0bb9161c3968c5ea3655fad6d83b0aa3e6311c
Stefan Buck <dev@stefanbuck.com>
2018-08-08 22:47:26 +0200

    Allow to set global flag when building RegExp

---
+++ packages/helper-regex-builder/__tests__/index.js
@@ -0,0 +1,19 @@
+import { jsonRegExValue, jsonRegExKeyValue } from '../index';
+
+describe('helper-regex-builder', () => {
+  it('jsonRegExValue', () => {
+    expect(jsonRegExValue('foo', 'bar')).toEqual(/"foo"\s*:\s*("bar")/);
+    expect(jsonRegExValue('foo', 'bar', false)).toEqual(/"foo"\s*:\s*("bar")/);
+    expect(jsonRegExValue('foo', 'bar', true)).toEqual(/"foo"\s*:\s*("bar")/g);
+  });
+
+  it('jsonRegExKeyValue', () => {
+    expect(jsonRegExKeyValue('foo', 'bar')).toEqual(/("foo")\s*:\s*("bar")/);
+    expect(jsonRegExKeyValue('foo', 'bar', false)).toEqual(
+      /("foo")\s*:\s*("bar")/,
+    );
+    expect(jsonRegExKeyValue('foo', 'bar', true)).toEqual(
+      /("foo")\s*:\s*("bar")/g,
+    );
+  });
+});
