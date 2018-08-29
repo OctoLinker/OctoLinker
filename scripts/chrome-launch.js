3da6100e2b121a641cd3a7abf900d091f5dd3ad3
leomoty <leomoty@gmail.com>
2018-08-29 19:04:20 -0300

    Fix yarn chrome-open to run in Windows (#508)
    Fixes #507, unsure if you want to keep these *-open around, but in Windows the relative dir is not resolved.
    
    ### Checklist:
    
    - [ ] If this PR is a new feature, please provide at least one example link
    - [ ] Make sure all of the significant new logic is covered by tests

--- scripts/chrome-launch.js
+++ scripts/chrome-launch.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 const chromeLaunch = require('chrome-launch'); // eslint-disable-line import/no-extraneous-dependencies
+const path = require('path');
 
 const url = 'https://github.com/OctoLinker/OctoLinker/blob/master/package.json';
-const args = ['--load-extension=./dist'];
+const dist = path.resolve(__dirname, '..', 'dist');
+const args = [`--load-extension=${dist}`];
 
 chromeLaunch(url, { args });
 console.log('A new instance of Chrome should now be open in the background.'); // eslint-disable-line no-console
