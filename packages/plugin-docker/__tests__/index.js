128754c0cd45961be65e17daaa5b02720f29f6df
Matt <mdg408@nyu.edu>
2018-09-02 20:47:30 -0400

    Switch back to expect instead of assert in Docker plugin tests

--- packages/plugin-docker/__tests__/index.js
+++ packages/plugin-docker/__tests__/index.js
@@ -1,46 +1,45 @@
-import assert from 'assert';
 import dockerImage from '../index';
 
 describe('docker-image', () => {
   const path = '/blob/path/dummy';
 
   it('resolves foo to https://hub.docker.com/_/foo', () => {
-    assert.deepEqual(dockerImage.resolve(path, ['foo']), [
+    expect(dockerImage.resolve(path, ['foo'])).toEqual([
       '{BASE_URL}/blob/path/foo',
       'https://hub.docker.com/_/foo',
     ]);
   });
 
   it('resolves foo:1.2.3 to https://hub.docker.com/_/foo', () => {
-    assert.deepEqual(dockerImage.resolve(path, ['foo:1.2.3']), [
+    expect(dockerImage.resolve(path, ['foo:1.2.3'])).toEqual([
       '{BASE_URL}/blob/path/foo:1.2.3',
       'https://hub.docker.com/_/foo',
     ]);
   });
 
   it('resolves foo:1.2.3-alpha to https://hub.docker.com/_/foo', () => {
-    assert.deepEqual(dockerImage.resolve(path, ['foo:1.2.3-alpha']), [
+    expect(dockerImage.resolve(path, ['foo:1.2.3-alpha'])).toEqual([
       '{BASE_URL}/blob/path/foo:1.2.3-alpha',
       'https://hub.docker.com/_/foo',
     ]);
   });
 
   it('resolves foo/bar to https://hub.docker.com/r/foo/bar', () => {
-    assert.deepEqual(dockerImage.resolve(path, ['foo/bar']), [
+    expect(dockerImage.resolve(path, ['foo/bar'])).toEqual([
       '{BASE_URL}/blob/path/foo/bar',
       'https://hub.docker.com/r/foo/bar',
     ]);
   });
 
   it('resolves foo/bar:1.2.3 to https://hub.docker.com/r/foo/bar', () => {
-    assert.deepEqual(dockerImage.resolve(path, ['foo/bar:1.2.3']), [
+    expect(dockerImage.resolve(path, ['foo/bar:1.2.3'])).toEqual([
       '{BASE_URL}/blob/path/foo/bar:1.2.3',
       'https://hub.docker.com/r/foo/bar',
     ]);
   });
 
   it('resolves foobar.sh to "{BASE_URL}/blob/path/foobar.sh" ', () => {
-    assert.deepEqual(dockerImage.resolve(path, ['foobar.sh']), [
+    expect(dockerImage.resolve(path, ['foobar.sh'])).toEqual([
       '{BASE_URL}/blob/path/foobar.sh',
       'https://hub.docker.com/_/foobar.sh',
     ]);
