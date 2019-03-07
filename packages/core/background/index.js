b086f21c4b1d59ec89b4cbb7b569224d108fb7d0
Stefan Buck <dev@stefanbuck.com>
2019-03-07 23:12:53 +0100

    Open settings dialog rather than being a new tab

--- packages/core/background/index.js
+++ packages/core/background/index.js
@@ -8,13 +8,5 @@ storage.load().then(() => {
 chrome.runtime.onMessage.addListener(({ action }) => {
   if (action !== 'openSettings') return;
 
-  const optionsUrl = chrome.extension.getURL('options.html');
-
-  chrome.tabs.query({ url: optionsUrl }, tabs => {
-    if (tabs.length) {
-      chrome.tabs.update(tabs[0].id, { active: true });
-    } else {
-      chrome.tabs.create({ url: optionsUrl });
-    }
-  });
+  chrome.runtime.openOptionsPage();
 });
