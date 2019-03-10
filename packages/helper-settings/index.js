00a1d3adbd192c543c564f309867d75c611b263f
Stefan Buck <github@stefanbuck.com>
2019-03-10 21:26:00 +0100

    Stop using sync API for settings in favor of local storage (#544)

--- packages/helper-settings/index.js
+++ packages/helper-settings/index.js
@@ -16,29 +16,17 @@ export const set = async (key, value) => {
     [key]: value,
   };
 
-  try {
-    return await browser.storage.sync.set(data);
-  } catch (err) {
-    return browser.storage.local.set(data);
-  }
+  return browser.storage.local.set(data);
 };
 
-export const save = async data => {
-  try {
-    return await browser.storage.sync.set(data);
-  } catch (err) {
-    return browser.storage.local.set(data);
-  }
-};
+export const save = async data => browser.storage.local.set(data);
 
 export const load = async () => {
-  let data;
+  // Clear legacy storage strategy without migration
+  // Can be removed at the end of April 2019
+  await browser.storage.sync.clear();
 
-  try {
-    data = await browser.storage.sync.get(null);
-  } catch (err) {
-    data = await browser.storage.local.get(null);
-  }
+  const data = await browser.storage.local.get(null);
 
   Object.assign(store, defaults, data);
 
