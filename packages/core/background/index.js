8fc60a06f8c24b4c170875dddf194acd3f5b57e2
Stefan Buck <dev@stefanbuck.com>
2019-03-15 22:53:24 +0100

    Remove not longer needed new tab background job

--- packages/core/background/index.js
+++ packages/core/background/index.js
@@ -1,10 +1,3 @@
-import * as storage from '@octolinker/helper-settings';
-import newTab from './newTab';
-
-storage.load().then(() => {
-  newTab();
-});
-
 chrome.runtime.onMessage.addListener(({ action }) => {
   if (action !== 'openSettings') return;
 
