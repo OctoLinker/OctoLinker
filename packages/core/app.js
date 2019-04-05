1ea99c802ba1a9cb1e85160e4d1eb42118605b68
Stefan Buck <dev@stefanbuck.com>
2019-04-05 22:17:44 +0200

    Rerun octolinker after progressive-container has loaded

--- packages/core/app.js
+++ packages/core/app.js
@@ -1,7 +1,96 @@
+import injection from 'github-injection';
+import BlobReader from '@octolinker/blob-reader';
+import insertLink from '@octolinker/helper-insert-link';
 import * as storage from '@octolinker/helper-settings';
-import OctoLinker from './octo-linker.js';
+import helperSortUrls from '@octolinker/helper-sort-urls';
+import normaliseResolverResults from '@octolinker/helper-normalise-resolver-results';
+import notification from './notification';
+import Plugins from './plugin-manager.js';
+import debugMode from './debug-mode.js';
+import loader from './loader.js';
+import * as loadPlugins from './load-plugins';
 
-storage.load().then(() => {
-  const octoLinker = new OctoLinker();
-  octoLinker.init();
-});
+const blobReader = new BlobReader();
+const pluginManager = new Plugins(loadPlugins);
+
+async function run(rootElement) {
+  const blobs = blobReader.read(rootElement);
+
+  let matches = [];
+  for (const blob of blobs) {
+    const plugins = pluginManager.get(blob.path, blob.el.classList);
+
+    if (plugins.length) {
+      for (const plugin of plugins) {
+        if (plugin.needsContext && blob.isDiff) {
+          await blob.fetchBlob(); // eslint-disable-line no-await-in-loop
+          await blob.fetchParentBlob(); // eslint-disable-line no-await-in-loop
+        }
+
+        if (plugin.parseBlob) {
+          matches = matches.concat(plugin.parseBlob(blob));
+        } else if (plugin.getLinkRegexes) {
+          for (const regex of [].concat(plugin.getLinkRegexes(blob))) {
+            matches = matches.concat(insertLink(blob, regex, plugin));
+          }
+        }
+      }
+    }
+  }
+
+  matches = matches
+    .filter(result => result !== undefined)
+    .map(({ link, urls }) => {
+      const urlsSorted = helperSortUrls(urls, link.innerText);
+
+      return {
+        link,
+        urls: normaliseResolverResults(urlsSorted),
+      };
+    });
+
+  // Prefetch live resolver results in background
+  requestIdleCallback(() => {
+    loader(matches);
+  });
+}
+
+function watch(viewSpy) {
+  const elements = [
+    ...document.getElementsByClassName('js-diff-load-container'),
+    ...document.getElementsByClassName('js-diff-progressive-container'),
+  ];
+
+  elements.forEach(element => {
+    viewSpy.observe(element, {
+      childList: true,
+    });
+  });
+}
+
+function init() {
+  debugMode(storage.get('debugMode'));
+  notification();
+
+  injection(() => {
+    if (!blobReader.hasBlobs()) {
+      return false;
+    }
+
+    run(document);
+  });
+
+  const viewSpy = new MutationObserver(mutationRecords => {
+    console.log('callback'); // check how often this gets invoked
+    mutationRecords.forEach(mutationRecord => {
+      if (mutationRecord.addedNodes.length > 0) {
+        run(this, mutationRecord.target);
+        watch(viewSpy);
+      }
+    });
+  });
+
+  watch(viewSpy);
+}
+
+storage.load().then(init);
