c790bc9fcab3f231936ac376945ac3761628c7ed
Stefan Buck <dev@stefanbuck.com>
2019-06-20 00:51:20 +0200

    Include @babel/plugin-transform-runtime for Jest

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,21 @@
-module.exports = {
-  env: {
-    test: {
-      presets: ['@babel/preset-env'],
-    },
-  },
-  plugins: [
+module.exports = api => {
+  const isTest = api.env('test');
+
+  const plugins = [
     '@babel/plugin-proposal-object-rest-spread',
     ['@babel/transform-react-jsx', { pragma: 'h' }],
-  ],
+  ];
+
+  if (isTest) {
+    plugins.push('@babel/plugin-transform-runtime');
+  }
+
+  return {
+    env: {
+      test: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+    plugins,
+  };
 };
