0c1e1f9e7e476a4a435c7858692c7653b8e70c28
Stefan Buck <dev@stefanbuck.com>
2019-08-02 20:09:37 +0200

    add e2e test

--- e2e/fixtures/javascript/index.js
+++ e2e/fixtures/javascript/index.js
@@ -3,3 +3,6 @@ require('fs');
 
 // @OctoLinkerResolve(https://github.com/lodash/lodash)
 require('lodash');
+
+// @OctoLinkerResolve(https://deno.land/x/std/fs/mod.ts)
+import * as fs from "https://deno.land/x/std/fs/mod.ts";
