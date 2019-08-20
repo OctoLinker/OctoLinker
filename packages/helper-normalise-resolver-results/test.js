8fb53f8c957c823362eac8defd91e2fb9a29f800
Stefan Buck <github@stefanbuck.com>
2019-08-20 13:34:36 +0200

    Fix prettier formatting

--- packages/helper-normalise-resolver-results/test.js
+++ packages/helper-normalise-resolver-results/test.js
@@ -8,7 +8,10 @@ describe('normaliseResolverResults', () => {
     '{BASE_URL}/foo/bar/blob/1ab8cfd3b65d3b43335130d6cefbf8c62482680f/file.js',
     'https://foosearch.org/',
     ['{BASE_URL}/foo/bar/blob/master/file.js'],
-    ['{BASE_URL}/foo/bar/blob/master/file.js', '{BASE_URL}/foo/bar/blob/master/file.js'],
+    [
+      '{BASE_URL}/foo/bar/blob/master/file.js',
+      '{BASE_URL}/foo/bar/blob/master/file.js',
+    ],
     { registry: 'npm', target: 'foo' },
     () => {},
   ])('converts %s', url => {
