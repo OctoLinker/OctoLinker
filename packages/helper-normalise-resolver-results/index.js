fdf79658734eec9b6975c7c2ada75ebd13684d78
Stefan Buck <github@stefanbuck.com>
2019-10-16 12:42:26 +0200

    Remove invalid ping urls before calling resolver api (#688)

--- packages/helper-normalise-resolver-results/index.js
+++ packages/helper-normalise-resolver-results/index.js
@@ -2,6 +2,14 @@ import ghParse from 'github-url-parse';
 
 const BASE_URL = 'https://github.com';
 
+// https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url/22648406#22648406
+function isURL(str) {
+  const urlRegex =
+    '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+  const url = new RegExp(urlRegex, 'i');
+  return str.length < 2083 && url.test(str);
+}
+
 // Resource within this repositroy
 const internal = url => {
   const fullUrl = url.replace('{BASE_URL}', BASE_URL);
@@ -18,10 +26,18 @@ const internal = url => {
 };
 
 // An external url like a documenation page
-const external = url => ({
-  type: 'ping',
-  target: url.replace('{BASE_URL}', BASE_URL),
-});
+const external = url => {
+  const target = url.replace('{BASE_URL}', BASE_URL);
+
+  if (!isURL(target)) {
+    return {};
+  }
+
+  return {
+    type: 'ping',
+    target,
+  };
+};
 
 // Async resolver
 const func = handler => ({
