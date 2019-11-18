b5e908a411708a818c9882ccb37145fbbd1ccc08
Stefan Buck <github@stefanbuck.com>
2019-11-19 00:36:09 +0100

    The line number should start at 1 (#710)

--- packages/helper-insert-link/__tests__/get-position.js
+++ packages/helper-insert-link/__tests__/get-position.js
@@ -2,6 +2,7 @@ import getPosition from '../get-position';
 
 describe('get-position', () => {
   const DEFAULT_REGEX = /(hello)/g;
+  const REGEX_MULTI_LINES = /START\n(hello)\nEND/g;
   const REGEX_QUOTES = /(["']hello["'])/g;
 
   test.each([
@@ -9,6 +10,9 @@ describe('get-position', () => {
     ['foo hello', DEFAULT_REGEX],
     ['foo hello bar', DEFAULT_REGEX],
     ['foo\nhello bar', DEFAULT_REGEX],
+    ['START\nhello\nEND', REGEX_MULTI_LINES],
+    ['foo\nSTART\nhello\nEND', REGEX_MULTI_LINES],
+    ['foo\nSTART\nhello\nEND', REGEX_MULTI_LINES],
     ['hello "hello"', REGEX_QUOTES],
     [`hello 'hello'`, REGEX_QUOTES],
     ['foo hello "hello"', REGEX_QUOTES],
@@ -18,9 +22,9 @@ describe('get-position', () => {
     const result = getPosition(input, regex);
     const [{ lineNumber, startPos, endPos }] = result;
 
-    expect(input.split('\n')[lineNumber].substring(startPos, endPos)).toEqual(
-      'hello',
-    );
+    expect(
+      input.split('\n')[lineNumber - 1].substring(startPos, endPos),
+    ).toEqual('hello');
     expect(result).toMatchSnapshot();
   });
 });
