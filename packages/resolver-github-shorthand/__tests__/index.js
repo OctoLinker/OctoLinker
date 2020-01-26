49be306c492cae96930cfa16d163dc31076ecc60
Stefan Buck <github@stefanbuck.com>
2020-01-26 21:26:31 +0100

    Add support for github: in package.json (#794)

---
+++ packages/resolver-github-shorthand/__tests__/index.js
@@ -0,0 +1,10 @@
+import resolver from '../index';
+
+describe('resolver-github-shorthand', () => {
+  test.each([['https://github.com/foo/bar'], ['github:foo/bar']])(
+    'resolves "%s" to https://github.com/foo/bar',
+    target => {
+      expect(resolver({ target })).toEqual('https://github.com/foo/bar');
+    },
+  );
+});
