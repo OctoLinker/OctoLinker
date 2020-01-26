fb749d2a8f3a0c153ce3b8da1f32b19bfc46590d
Stefan Buck <dev@stefanbuck.com>
2020-01-26 20:00:48 +0100

    Bypass OctoLinker API for nuget urls
    By default OctoLinker validates external URLs via https://octolinker-api.now.sh/ to ensure the linked URL is valid and reachable. This obviously slows down the startup since OctoLinker needs to wait for the API response. There is a special resolver called `resolver-trusted-url` which allows you to opt-out from this behaviour. This is useful for documenation pages or registry urls where you can ensure the right url.

--- packages/plugin-dot-net/__tests__/index.js
+++ packages/plugin-dot-net/__tests__/index.js
@@ -4,14 +4,16 @@ describe('dotNet', () => {
   const path = '/blob/path/dummy';
 
   it('resolves EntityFramework to https://www.nuget.org/packages/EntityFramework', () => {
-    expect(dotNet.resolve(path, ['EntityFramework'])).toBe(
-      'https://www.nuget.org/packages/EntityFramework',
-    );
+    expect(dotNet.resolve(path, ['EntityFramework'])).toEqual({
+      target: 'https://www.nuget.org/packages/EntityFramework',
+      type: 'trusted-url',
+    });
   });
 
   it('resolves Stanford.NLP.CoreNLP to https://www.nuget.org/packages/Stanford.NLP.CoreNLP', () => {
-    expect(dotNet.resolve(path, ['Stanford.NLP.CoreNLP'])).toBe(
-      'https://www.nuget.org/packages/Stanford.NLP.CoreNLP',
-    );
+    expect(dotNet.resolve(path, ['Stanford.NLP.CoreNLP'])).toEqual({
+      target: 'https://www.nuget.org/packages/Stanford.NLP.CoreNLP',
+      type: 'trusted-url',
+    });
   });
 });
