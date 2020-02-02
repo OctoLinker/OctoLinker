021b7b5e9bd7c9b6942a8820a57294e5537be91c
Brian Surowiec <brian@13degrees.com>
2020-02-02 15:57:11 -0500

    Add support for .net references (#808)
    * Add support for .net references
    
    * Use shorter regex
    
    * Fix file formatting

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -201,7 +201,7 @@ export const JAVA_IMPORT = regex`
 `;
 
 export const NET_PROJ_PACKAGE = regex`
-  <(PackageReference|DotNetCliToolReference|FrameworkReference)
+  <(DotNetCliTool|Framework|Package)?Reference
   \s+
   .*
   (Include|Update)=${captureQuotedWord}
