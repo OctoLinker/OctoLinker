234cd9bcc8585a28f62fedbc25186ad2ff955c05
Stefan Buck <dev@stefanbuck.com>
2020-03-08 20:58:56 +0100

    Do not pare suggested code changes block

--- packages/blob-reader/index.js
+++ packages/blob-reader/index.js
@@ -11,6 +11,10 @@ function parseBlob(el) {
   const path = getPath(el);
   const lines = readLines(el);
 
+  if (!lines.length) {
+    return;
+  }
+
   // Does not work if left side is empty (eg new files) https://github.com/OctoLinker/OctoLinker/commit/b97dfbfdbf3dee5f4836426e6dac6d6f473461db?diff=split#diff-e56633f72ecc521128b3db6586074d2c
   const isDiff =
     lines.filter(({ side }) => ['left', 'right'].includes(side)).length > 0;
@@ -55,9 +59,11 @@ export default class BlobReader {
 
   read(rootElement) {
     return [].concat(
-      ...getBlobWrapper(rootElement).map(el => {
-        return parseBlob(el);
-      }),
+      ...getBlobWrapper(rootElement)
+        .map(el => {
+          return parseBlob(el);
+        })
+        .filter(Boolean),
     );
   }
 }
