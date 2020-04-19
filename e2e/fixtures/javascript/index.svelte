eb48159b5c767b93a137a48bbf5838e315bf910f
Brian Surowiec <brian@13degrees.com>
2020-04-19 16:08:40 -0400

    Add support for .svelte files (#898)
    * Add support for .svelte files
    
    * More svelte tests

---
+++ e2e/fixtures/javascript/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  // @OctoLinkerResolve(https://github.com/sveltejs/svelte)
+  import { onMount } from 'svelte';
+
+  // @OctoLinkerResolve(<root>/javascript/relative/gentle-resonance.js)
+  import * as gr from './relative/gentle-resonance';
+
+  // @OctoLinkerResolve(<root>/javascript/relative/proud-tooth.js)
+  import * as pt from './relative/proud-tooth.js';
+
+  // @OctoLinkerResolve(<root>/javascript/relative/proud-tooth.svelte)
+  import * as pt from './relative/proud-tooth.svelte';
+</script>
