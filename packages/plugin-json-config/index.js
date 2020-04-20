fe7a6e0d52dbe37792eeac68fbd5ba64bd5d4c46
Brian Surowiec <brian@13degrees.com>
2020-04-19 22:32:52 -0400

    Add support for json config files

---
+++ packages/plugin-json-config/index.js
@@ -0,0 +1,41 @@
+import insertLink from '@octolinker/helper-insert-link';
+import processJSON from '@octolinker/helper-process-json';
+import {
+  jsonRegExValue,
+  jsonRegExArrayValue,
+} from '@octolinker/helper-regex-builder';
+import liveResolverQuery from '@octolinker/resolver-live-query';
+
+function linkDependency(blob, key, value) {
+  let regex;
+  
+  if (Number.isNaN(key)) {
+    regex = jsonRegExValue(key, value, true);
+  } else {
+    regex = jsonRegExArrayValue(value);
+  }
+
+  return insertLink(blob, regex, this);
+}
+
+export default {
+  name: 'JsonConfig',
+  needsContext: true,
+
+  resolve(path, values) {
+    return liveResolverQuery({ type: 'npm', target: values[0] });
+  },
+
+  getPattern() {
+    return {
+      pathRegexes: [/\.stylelintrc\.json$/, /tsconfig\.json$/],
+      githubClasses: [],
+    };
+  },
+
+  parseBlob(blob) {
+    return processJSON(blob, this, {
+      '$.extends': linkDependency,
+    });
+  },
+};
