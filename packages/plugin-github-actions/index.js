77dea1084a7bafafcdc752b5270825bb3c5ecd25
Brian Surowiec <brian@13degrees.com>
2020-06-04 03:53:41 -0400

    Support local actions

--- packages/plugin-github-actions/index.js
+++ packages/plugin-github-actions/index.js
@@ -1,3 +1,4 @@
+import { join } from 'path';
 import { GITHUB_ACTIONS } from '@octolinker/helper-grammar-regex-collection';
 
 export default {
@@ -6,6 +7,12 @@ export default {
   resolve(path, [target]) {
     if (!path.includes('.github/workflows')) {
       return '';
+    }
+
+    if (target.startsWith('.')) {
+      const [, org, repo, , sha] = path.split('/');
+
+      return `{BASE_URL}/${join(org, repo, 'tree', sha, target)}`;
     }
 
     return `{BASE_URL}/${target}`;
