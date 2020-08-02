847e95b7cf51bac297c75f32a12cbc82ecc6f933
Stefan Buck <github@stefanbuck.com>
2020-08-02 22:55:40 +0200

    Remove unused css selector

--- packages/user-interface/index.js
+++ packages/user-interface/index.js
@@ -33,7 +33,7 @@ export const showNotification = ({
       <div class="octo-toast-image"></div>
       ${body}
     </span>
-      <button class="Toast-dismissButton js-octolinker-toast-close-${id}" type="button" aria-label="Close">
+      <button class="Toast-dismissButton" type="button" aria-label="Close">
         <svg class="octicon octicon-x" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path></svg>
       </button>
   </div>`;
