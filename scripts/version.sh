451961fd248cbf147c7dec1565cd619cff9d028f
Stefan Buck <github@stefanbuck.com>
2020-10-02 23:39:03 +0200

    Ensure release description is provided

---
+++ scripts/version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script will test if you have given a leap year or not.
+
+echo "Please summarize this release in one sentence"
+
+read note
+
+dot-json assets/manifest.json version $npm_package_version
+dot-json packages/core/package.json version $npm_package_version
+dot-json packages/core/package.json releaseDescription $note
+git add assets/manifest.json packages/core/package.json
