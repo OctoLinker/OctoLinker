9d839ab5a3a8666a9a09b10061f75863c66ab483
Stefan Buck <github@stefanbuck.com>
2020-10-07 22:13:54 +0200

    Handle whitespaces in release script

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,5 +6,5 @@ read note
 
 dot-json assets/manifest.json version $npm_package_version
 dot-json packages/core/package.json version $npm_package_version
-dot-json packages/core/package.json releaseDescription $note
+dot-json packages/core/package.json releaseDescription "$note"
 git add assets/manifest.json packages/core/package.json
