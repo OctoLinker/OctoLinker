ec96e913f9b4a40ce940233e68905661f12a65fe
edavidaja <edavidaja@hey.com>
2020-10-12 17:40:10 -0400

    case correction

--- e2e/fixtures/r/foo.R
+++ e2e/fixtures/r/foo.R
@@ -1,20 +1,20 @@
-# @octoLinkerResolve(https://github.com/r-lib/rlang)
+# @OctoLinkerResolve(https://github.com/r-lib/rlang)
 library(rlang)
 
-# @octoLinkerResolve(https://github.com/r-lib/rlang)
+# @OctoLinkerResolve(https://github.com/r-lib/rlang)
 library("rlang")
 
-# @octoLinkerResolve(https://github.com/r-lib/rlang)
+# @OctoLinkerResolve(https://github.com/r-lib/rlang)
 require(rlang, quietly = TRUE)
 
-# @octoLinkerResolve(https://github.com/r-lib/rlang)
+# @OctoLinkerResolve(https://github.com/r-lib/rlang)
 require("rlang")
 
-# @octoLinkerResolve(https://github.com/r-lib/rlang)
+# @OctoLinkerResolve(https://github.com/r-lib/rlang)
 requireNamespace("rlang", quietly = TRUE)
 
-# @octoLinkerResolve(https://github.com/r-lib/rlang)
+# @OctoLinkerResolve(https://github.com/r-lib/rlang)
 requireNamespace("rlang")
 
-# @octoLinkerResolve(https://github.com/r-lib/rlang)
+# @OctoLinkerResolve(https://github.com/r-lib/rlang)
 loadNamespace("rlang")
