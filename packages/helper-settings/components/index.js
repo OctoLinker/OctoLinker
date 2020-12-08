3d9781df952c205db6a05062205a4b52bee4f683
Stefan Buck <github@stefanbuck.com>
2020-12-08 21:13:39 +0100

    Show usage statistics on setttings page (#1129)
    * Show usage statistics on setttings page
    
    * Show usage statistics on setttings page

--- packages/helper-settings/components/index.js
+++ packages/helper-settings/components/index.js
@@ -2,3 +2,4 @@ import 'webext-base-css';
 
 export { default as Input } from './input';
 export { default as Checkbox } from './checkbox';
+export { default as Stats } from './stats';
---
+++ packages/helper-settings/components/stats.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+
+export default ({ counter }) => (
+  <div>
+    <h2>Statistics</h2>
+    Links followed: <strong>{counter}</strong>
+    <span className="note pt-4">
+      Your statistics are stored in your browser, we do not store them anywhere
+      else.
+    </span>
+  </div>
+);
