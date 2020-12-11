7856a0bcda25d04d0d06a3472f57f191631916ad
laundmo <laurinschmidt2001@gmail.com>
2020-12-11 14:31:01 +0100

    Add python e2 test cases.

---
+++ e2e/fixtures/python/library_imports.py
@@ -0,0 +1,4 @@
+# @OctoLinkerResolve(https://github.com/Rapptz/discord.py)
+from discord.ext import commands 
+# @OctoLinkerResolve(https://github.com/Rapptz/discord.py)
+from discord.ext.commands import Cog, Context, command 
