cce4385daea2542a98b7847395b4d1c79ec31953
laundmo <laurinschmidt2001@gmail.com>
2020-12-12 00:41:43 +0100

    Add OctoLinkerResolveAbove where relevant.

--- e2e/fixtures/python/relative/subpackage2/import_from_other.py
+++ e2e/fixtures/python/relative/subpackage2/import_from_other.py
@@ -3,10 +3,12 @@ from ..subpackage1.classes import MyClass1
 
 # @OctoLinkerResolve(<root>/python/relative/subpackage1/classes.py)
 from ...relative.subpackage1 import classes
-#    ^ should resolve folder        ^ should resolve file
+# @OctoLinkerResolveAbove(<root>/python/relative/subpackage1)
 
 # @OctoLinkerResolve(<root>/python/relative/subpackage1/)
 from ...relative import subpackage1
+# @OctoLinkerResolveAbove(<root>/python/relative)
 
 # @OctoLinkerResolve(<root>/python/relative/subpackage1/classes.py)
 from ..subpackage1 import classes
+# @OctoLinkerResolveAbove(<root>/python/subpackage1)
