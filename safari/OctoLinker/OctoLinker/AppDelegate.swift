a7bda7a930b6b84a6a35fe457b08d43ae95df33f
Federico Brigante <me@fregante.com>
2020-12-30 16:43:16 -0600

    Plain safari-web-extension-converter
    xcrun safari-web-extension-converter dist/ --project-location safari --app-name 'OctoLinker' --bundle-identifier 'com.stefanbuck.OctoLinker'

---
+++ safari/OctoLinker/OctoLinker/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  OctoLinker
+//
+//  Created by Rico on 30/12/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ safari/OctoLinker/OctoLinker/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  OctoLinker
+//
+//  Created by Rico on 30/12/20.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+import SafariServices.SFSafariExtensionManager
+
+let appName = "OctoLinker"
+let extensionBundleIdentifier = "com.stefanbuck.OctoLinker.Extension"
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = appName
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
+            guard let state = state, error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                if (state.isEnabled) {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
+                } else {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
+                }
+            }
+        }
+    }
+
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+            guard error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+
+}
