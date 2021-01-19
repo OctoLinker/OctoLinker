8b50ca7074ae1a9d9aab4e09b0d80a8d30b6055d
Stefan Buck <github@stefanbuck.com>
2021-01-19 23:05:33 +0100

    Update Safari version when using npm version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Please summarize this release in one sentence"
-
-read note
-
+sed -E "s/MARKETING_VERSION = [0-9.]+/MARKETING_VERSION = $npm_package_version/" safari/Shared.xcconfig | tee safari/Shared.xcconfig
 dot-json assets/manifest.json version $npm_package_version
 dot-json packages/core/package.json version $npm_package_version
+
+echo "Please summarize this release in one sentence"
+read note
 dot-json packages/core/package.json releaseDescription "$note"
-git add assets/manifest.json packages/core/package.json
+
+git add assets/manifest.json packages/core/package.json safari/Shared.xcconfig
