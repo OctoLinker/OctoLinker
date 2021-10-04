025aa24e2e213352408eeca15ed72b74a2562c79
Stefan Buck <github@stefanbuck.com>
2021-10-04 19:40:14 +0200

    Update failing End 2 End tests with new target url (#1372)

--- e2e/fixtures/go/foo.go
+++ e2e/fixtures/go/foo.go
@@ -6,10 +6,10 @@ import "context"
 // @OctoLinkerResolve(https://golang.org/pkg/math/big)
 import "math/big"
 
-// @OctoLinkerResolve(https://github.com/kubernetes/apimachinery/tree/master/pkg/api/errors)
+// @OctoLinkerResolve(https://golang.org/pkg/k8s.io/apimachinery/pkg/api/errors)
 import "k8s.io/apimachinery/pkg/api/errors"
 
-// @OctoLinkerResolve(https://github.com/kubernetes/api/tree/master/core/v1)
+// @OctoLinkerResolve(https://golang.org/pkg/k8s.io/api/core/v1)
 import corev1 "k8s.io/api/core/v1"
 
 // @OctoLinkerResolve(https://github.com/ethereum/go-ethereum/tree/master/common)
--- e2e/fixtures/java/rough-snowflake.java
+++ e2e/fixtures/java/rough-snowflake.java
@@ -4,5 +4,5 @@ import java.util.List;
 // @OctoLinkerResolve(http://fasterxml.github.io/jackson-databind/javadoc/2.9/com/fasterxml/jackson/databind/JsonNode.html)
 import com.fasterxml.jackson.databind.JsonNode;
 
-// @OctoLinkerResolve(https://docs.spring.io/spring/docs/5.1.7.RELEASE/javadoc-api/org/springframework/stereotype/Component.html)
+// @OctoLinkerResolve(https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/stereotype/Component.html)
 import org.springframework.stereotype.Component;
