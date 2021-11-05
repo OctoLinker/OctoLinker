b65d1385294e3570feb29229aad54df91fa04db1
Luca Perret <perret.luca@gmail.com>
2021-11-05 21:36:54 +0100

    feat(plugin-solidity): add npm resolver

--- packages/plugin-solidity/__tests__/index.js
+++ packages/plugin-solidity/__tests__/index.js
@@ -6,8 +6,29 @@ describe('Solidity', () => {
 
   it('resolves link when target has a file extension', () => {
     assert.deepEqual(
-      Solidity.resolve(path, ['foo.sol']),
+      Solidity.resolve(path, ['./foo.sol']),
       '{BASE_URL}/octo/foo.sol',
     );
+  });
+
+  it("resolves 'foo/bar.sol' like 'foo'", () => {
+    const type = 'npm';
+    expect(Solidity.resolve(path, ['foo/bar.sol'], { type })).toEqual(
+      Solidity.resolve(path, ['foo'], { type }),
+    );
+  });
+
+  it("resolves '@openzeppelin/contracts/token/ERC721/ERC721.sol' to '@openzeppelin/contracts'", () => {
+    const type = 'npm';
+    expect(
+      Solidity.resolve(
+        path,
+        ['@openzeppelin/contracts/token/ERC721/ERC721.sol'],
+        { type },
+      ),
+    ).toEqual({
+      registry: 'npm',
+      target: '@openzeppelin/contracts',
+    });
   });
 });
--- packages/plugin-solidity/index.js
+++ packages/plugin-solidity/index.js
@@ -1,11 +1,28 @@
 import { SOLIDITY_IMPORT } from '@octolinker/helper-grammar-regex-collection';
 import relativeFile from '@octolinker/resolver-relative-file';
+import liveResolverQuery from '@octolinker/resolver-live-query';
+
+function getTopModuleName(target) {
+  const isScoped = target.startsWith('@');
+  const numComponents = isScoped ? 2 : 1;
+  const topModuleName = target.split('/').slice(0, numComponents).join('/');
+  return topModuleName;
+}
 
 export default {
   name: 'Solidity',
 
   resolve(path, [target]) {
-    return relativeFile({ path, target });
+    let result;
+    if (target.match(/^\.\.?[\\|\/]?/) !== null) {
+      result = relativeFile({ path, target });
+    } else {
+      result = liveResolverQuery({
+        type: 'npm',
+        target: getTopModuleName(target),
+      });
+    }
+    return result;
   },
 
   getPattern() {
