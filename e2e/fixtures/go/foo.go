236355213be255e8f8cd2e099ab70ed236b532bd
Brian Surowiec <brian@13degrees.com>
2022-04-05 11:00:20 -0400

    Update resolve url

--- e2e/fixtures/go/foo.go
+++ e2e/fixtures/go/foo.go
@@ -9,7 +9,7 @@ import "math/big"
 // @OctoLinkerResolve(https://golang.org/pkg/k8s.io/apimachinery/pkg/api/errors)
 import "k8s.io/apimachinery/pkg/api/errors"
 
-// @OctoLinkerResolve(https://golang.org/pkg/k8s.io/api/core/v1)
+// @OctoLinkerResolve(https://github.com/kubernetes/api/tree/master/core/v1)
 import corev1 "k8s.io/api/core/v1"
 
 // @OctoLinkerResolve(https://github.com/ethereum/go-ethereum/tree/master/common)
