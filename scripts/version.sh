2483f2f457ccc9e6de042a12a161ce58135f7571
Stefan Buck <github@stefanbuck.com>
2022-04-07 22:46:27 +0200

    New release flow (#1540)
    * Update release flow
    
    * fixup! Update release flow
    
    * Update .github/workflows/release.yml
    
    Co-authored-by: Brian Surowiec <brian@13degrees.com>
    
    Co-authored-by: Brian Surowiec <brian@13degrees.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,6 +2,3 @@
 
 sed -E "s/MARKETING_VERSION = [0-9.]+/MARKETING_VERSION = $npm_package_version/" safari/Shared.xcconfig | tee safari/Shared.xcconfig
 dot-json assets/manifest.json version $npm_package_version
-dot-json packages/core/package.json version $npm_package_version
-
-git add assets/manifest.json packages/core/package.json safari/Shared.xcconfig
