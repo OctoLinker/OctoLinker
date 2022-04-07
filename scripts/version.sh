8d21cc2e0649d5df173c8b7c418b5614430561f6
Stefan Buck <github@stefanbuck.com>
2022-04-07 23:07:05 +0200

    Fix version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sed -E "s/MARKETING_VERSION = [0-9.]+/MARKETING_VERSION = $npm_package_version/" safari/Shared.xcconfig | tee safari/Shared.xcconfig
-dot-json assets/manifest.json version $npm_package_version
+"$(npm bin)"/dot-json assets/manifest.json version $npm_package_version
